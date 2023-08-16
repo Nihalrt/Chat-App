ae54f070f18dff56e294e14857d10d01c6c44d29
Sai Nihal <109018562+Nihalrt@users.noreply.github.com>
2023-08-16 17:29:53 +0530

    started

---
+++ app/components/Button.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import clsx from 'clsx';
+
+interface ButtonProps{
+    type?: 'button' | 'submit' | 'reset' | undefined;
+    fullWidth?: boolean;
+    children?: React.ReactNode;
+    onClick?: () => void;
+    secondary?: boolean;
+    danger?: boolean;
+    disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    type,
+    fullWidth,
+    children,
+    onClick,
+    secondary,
+    danger,
+    disabled
+}) => {
+    return(
+        <div>
+            Hi Button
+        </div>
+    )
+}
+
+export default Button;
